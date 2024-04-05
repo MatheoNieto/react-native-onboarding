ba6f310c93f973c7b302902557c38260cfb04fbb
Mateo Nieto <matheo.developer@gmail.com>
2024-04-05 16:27:26 -0500

    for testing

--- src/elements/ButtonStep.tsx
+++ src/elements/ButtonStep.tsx
@@ -1,39 +1,45 @@
-import React, {FC} from "react";
-import { Pressable } from "react-native";
-import Icon from "react-native-vector-icons/AntDesign";
+import React, { FC } from 'react';
+import { Pressable } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 
-type PropsButtonStep ={
-	onPress: () => void;
-	colorIcon: string;
-	typeButton: "next" | "previous";
-	disabled?: boolean;
-	finish?: boolean;
-}
+type PropsButtonStep = {
+  onPress: () => void;
+  colorIcon: string;
+  type: 'next' | 'previous';
+  disabled?: boolean;
+  finish?: boolean;
+};
 
-const ButtonStep: FC<PropsButtonStep> = ({typeButton, finish, colorIcon, disabled, onPress}) => {
-	const renderButton = () => {
-		if (typeButton === "previous") {
-			return buttonBack();
-		}
+const ButtonStep: FC<PropsButtonStep> = ({
+  type: typeButton,
+  finish,
+  colorIcon,
+  disabled,
+  onPress,
+}) => {
+  const renderButton = () => {
+    if (typeButton === 'previous') {
+      return buttonBack();
+    }
 
-		return buttonNext();
-	};
+    return buttonNext();
+  };
 
-	const buttonNext = () => {
-		if (finish) {
-			return <Icon name='checkcircleo' size={40} color={colorIcon} />;
-		}
-		return <Icon name='rightsquareo' size={40} color={colorIcon} />;
-	};
+  const buttonNext = () => {
+    if (finish) {
+      return <Icon name="checkcircleo" size={40} color={colorIcon} />;
+    }
+    return <Icon name="rightsquareo" size={40} color={colorIcon} />;
+  };
 
-	const buttonBack = () => {
-		return <Icon name='leftsquareo' size={40} color={colorIcon} />;
-	};
+  const buttonBack = () => {
+    return <Icon name="leftsquareo" size={40} color={colorIcon} />;
+  };
 
-	return (
-		<Pressable onPress={() => onPress()} disabled={disabled}>
-			{renderButton()}
-		</Pressable>
-	);
+  return (
+    <Pressable onPress={() => onPress()} disabled={disabled}>
+      {renderButton()}
+    </Pressable>
+  );
 };
 export default ButtonStep;
--- src/sections/FooterProgress.tsx
+++ src/sections/FooterProgress.tsx
@@ -1,94 +1,95 @@
-import React, { FC, useEffect } from "react";
-import { View, StyleSheet } from "react-native";
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 
-import StepIcon from "../elements/StepIcon";
-import ButtonStep from "../elements/ButtonStep";
-import {ActionsBoardingType} from "../types";
+import StepIcon from '../elements/StepIcon';
+import ButtonStep from '../elements/ButtonStep';
+import { ActionsBoardingType } from '../types';
+import { useBoarding } from '../store/hook';
+import { colorDefaultIcons } from '../store/provider';
 
-const FooterProgress:React.FC<ActionsBoardingType> = ({onFinish, onNext, onPrevious, footerStyle}) => {
-	useEffect(()=> {
-		setStepActive(stepActive)
-	},[stepActive])
+const FooterProgress: React.FC<ActionsBoardingType> = ({
+  onFinish,
+  onNext,
+  onPrevious,
+  footerStyle,
+}) => {
+  const { stepActive, isComplete, stepCount, onChangeStep, config } = useBoarding();
+  const colorIcon = config?.colorIcons ?? colorDefaultIcons;
 
-	const renderStepIcons = () => {
-		let step = [];
+  const renderStepIcons = () => {
+    let step = [];
 
-		if(stepCount < 1) {
-			return;
-		}
+    if (stepCount < 1) {
+      return;
+    }
 
-		for (let i = 0; i < stepCount; i++) {
-			const isCompletedStep =isComplete
-				? true
-				: i < stepActive;
+    for (let i = 0; i < stepCount; i++) {
+      const isCompletedStep = isComplete ? true : i < stepActive;
+      const isActiveStep = isComplete ? false : i == stepActive;
 
-			const isActiveStep = isComplete
-				? false
-				: i == stepActive;
+      step.push(
+        <StepIcon
+          key={i}
+          isCompletedStep={isCompletedStep}
+          isActiveStep={isActiveStep}
+          colorIcon={colorIcon}
+        />,
+      );
+    }
 
-			step.push(
-				<StepIcon
-					key={i}
-					isCompletedStep={isCompletedStep}
-					isActiveStep={isActiveStep}
-					colorIcon={colorIcons}
-				/>
-			);
-		}
+    return step;
+  };
 
-		return step;
-	};
+  const onNextStep = () => {
+    if (stepActive === stepCount - 1) {
+      onFinish && onFinish();
+      return;
+    }
 
-	const onNextStep = () => {
-		if (stepActive === stepCount - 1) {
-			onFinish && onFinish();
-			return;
-		}
+    onNext && onNext();
+    onChangeStep(stepActive + 1);
+  };
 
-		onNext && onNext();
-		setStepActive(stepActive + 1);
-	};
+  const onPreviousStep = () => {
+    onPrevious && onPrevious();
+    onChangeStep(stepActive - 1);
+  };
 
-	const onPreviousStep = () => {
-		onPrevious && onPrevious();
-		setStepActive(stepActive - 1);
-	};
+  if (!stepCount) {
+    return null;
+  }
 
-	if (!stepCount) {
-		return null;
-	}
-
-	return (
-		<View style={[styles.styleContent, containerStyle]}>
-			<ButtonStep
-				finish={stepActive === stepCount - 1}
-				onPress={() => onNextStep()}
-				colorIcon={colorIcons}
-				type='next'
-			/>
-			<View style={styles.stepIcons}>{renderStepIcons()}</View>
-			<ButtonStep
-				onPress={() => onPreviousStep()}
-				disabled={stepActive === 0}
-				colorIcon={colorIcons}
-				type='previous'
-			/>
-		</View>
-	);
-}
+  return (
+    <View style={StyleSheet.flatten([styles.styleContent, footerStyle])}>
+      <ButtonStep
+        finish={stepActive === stepCount - 1}
+        onPress={() => onNextStep()}
+        colorIcon={colorIcon}
+        type="next"
+      />
+      <View style={styles.stepIcons}>{renderStepIcons()}</View>
+      <ButtonStep
+        onPress={() => onPreviousStep()}
+        disabled={stepActive === 0}
+        colorIcon={colorIcon}
+        type="previous"
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-	stepIcons: {
-		flexDirection: "row",
-	},
-	styleContent: {
-		flexDirection: "row-reverse",
-		alignItems: "center",
-		justifyContent: "space-around",
-		backgroundColor: "#f9f8f2",
-		opacity: 1,
-		paddingVertical: 10,
-	},
+  stepIcons: {
+    flexDirection: 'row',
+  },
+  styleContent: {
+    flexDirection: 'row-reverse',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: '#f9f8f2',
+    opacity: 1,
+    paddingVertical: 10,
+  },
 });
 
 export default FooterProgress;
